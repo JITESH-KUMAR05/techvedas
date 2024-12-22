e4b9a7bf8722f92ff9137c3cfa668ebf18048159
jitesh <jitesh.kumar05official@gmail.com>
2024-12-22 17:21:03 +0530

    favicon added

--- my-blog-frontend/src/components/Header.jsx
+++ my-blog-frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-gray-800 text-white shadow-md h-full flex items-center">
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="text-2xl font-bold">
-          <a href="/">My Blog</a>
+          <a href="/">TechVedas</a>
         </div>
         <nav className="space-x-4">
           <a href="/" className="hover:text-gray-400">Home</a>
